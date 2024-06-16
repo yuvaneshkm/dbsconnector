8acb39bd84db3b290c7838d44def598249812246
yuvaneshkm <yuvaneshkm05@gmail.com>
2024-06-16 22:48:45 +0530

    'updated'

---
+++ src/dbsconnector/databases.py
@@ -0,0 +1,43 @@
+# importing necessary libraries
+import pandas as pd
+from pathlib import Path
+from pymysql import connect
+from pymongo.mongo_client import MongoClient
+
+
+# load csv file:
+def load_csv(filepath:str, delimiter:str):
+    filepath = Path(filepath)
+    return pd.read_csv(filepath, delimiter=delimiter)
+    
+# load excel sheet
+def load_excelsheet(filepath:str, sheet_name:str):
+    filepath = Path(filepath)
+    return pd.read_excel(filepath, sheet_name=sheet_name)
+
+# load google sheet 
+def load_gsheet(gsheet_id:str, sheet_name:str):
+    base_url = 'https://docs.google.com/spreadsheets/d'
+    sheet_csv = 'gviz/tq?tqx=out:csv&sheet='
+    url = f'{base_url}/{gsheet_id}/{sheet_csv}{sheet_name}'
+    url = Path(url)
+    return pd.read_csv(url)
+
+# load mysql data:
+def load_mysqldata(host:str, user:str, password:str, database:str, table:str):
+    db = connect(host=host, user=user, passwd=password, database=database)
+    cur = db.cursor()
+    query = f'select * from {table}'
+    return pd.read_sql(query, db)
+
+# load mongodb data:
+def load_mongodbdata(host:str, database:str, collection:str):
+    client = MongoClient(host)
+    db = client[database]
+    col = db[collection]
+    records = col.find()
+    data = list(records)
+    df = pd.DataFrame(data)
+    df.drop('_id', axis=1, inplace=True)
+    return df
+
